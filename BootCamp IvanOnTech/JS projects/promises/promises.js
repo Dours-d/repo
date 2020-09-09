71e4632b8bcdf536e44241d757cfe8726597969d
dours-d <dours_d@protonmail.com>
2020-09-09 18:08:49 +0200

    bootcamp

--- BootCamp IvanOnTech/JS projects/promises/promises.js
+++ BootCamp IvanOnTech/JS projects/promises/promises.js
@@ -1,27 +1,41 @@
-//BLUEPRINT
-var promise = new Promise(function(resolve,reject){
-	setTimeout(resolve,2000);
+	src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"
 
-});
+	
+//$(document).ready(function(){
+  //    $("button").click(function(){
+    //    $("h1").slideUp(function(){
+         
+function toggleContent(){
+	var contentId = document.getElementById("content")
 
-promise.then(function(){
-	console.log("Ready?");
+	contentId.style.display == "block" ? 
+	contentId.style.display = "none" : 
+	contentId.style.display = "block";
+};
 
+function constructTimeoutPromise(){
 	return new Promise(function(resolve,reject){
 		setTimeout(resolve,2000);
 	});
+}
+
+//BLUEPRINT
+constructTimeoutPromise().then(function(){
+	console.log("Ready?");
+	return constructTimeoutPromise();
+	
+	
 }).then(function(){
 	console.log("Steady!");
-
-	return new Promise(function(resolve,reject){
-		setTimeout(resolve,2000);
-	});
+	return constructTimeoutPromise();
+	
+	
 }).then(function(){
 	console.log("Eddy!");
+	return constructTimeoutPromise();
+	 
 
-	return new Promise(function(resolve,reject){
-		setTimeout(resolve,1800);
-	});
 }).then(function(){
 	console.log("GO!");
+	
 });
