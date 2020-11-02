7db78234882d2ccf027df9db76fbb3b82f2e6eed
Dours <dours_d@protonmail.com>
2020-11-02 18:29:37 +0100

    Create memory-assignment.sol

---
+++ BootCamp IvanOnTech/Solidity Eth programming/Assingments/memory-assignment.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.1;
+contract MemoryAndStorage {
+
+    mapping(uint => User) users;
+
+    struct User{
+        uint id;
+        uint balance;
+    }
+
+    function addUser(uint id, uint balance) public {
+        users[id] = User(id, balance);
+    }
+
+    function updateBalance(uint id, uint balance) public returns (bool success){
+         users[id].balance = balance;
+         return success;
+    }
+
+    function getBalance(uint id) view public returns (uint) {
+        return users[id].balance;
+    }
+
+}
